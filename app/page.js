c1dd54584bd23d6284c3602e03635a2a608f61f2
Bahng Ji Hoon <wpxm2003@naver.com>
2025-07-12 08:48:14 +0900

    페이지 이동 예제제

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div>
 
-    </div>
+  return (
+    <main>
+      
+      <h1 className="title">Programming Log</h1>
+      <p className="title-sub">by dev bahng</p>
+    </main>
   );
 }
