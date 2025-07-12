4a79161f9226be3ea3ead6992f99e418d555514d
Bahng Ji Hoon <wpxm2003@naver.com>
2025-07-12 10:23:48 +0900

    수량 조절 기능 추가가

--- app/list/page.jsx
+++ app/list/page.jsx
@@ -1,8 +1,23 @@
+'use client';
+
 import Image from "next/image";
 // import 이미지 from '/public/food0.png';
+import { useState } from "react";
+
 
 export default function List() {
     let products = ['tomato', 'pasta', 'coconut'];
+    let [count, setCount] = useState(0);
+
+    function up() {
+        setCount(count + 1);
+    }
+
+    function down() {
+        if (count > 0) {
+            setCount(count -1 );
+        }
+    }
 
     return (
         <div>
@@ -13,6 +28,10 @@ export default function List() {
                         <h4>{product} $40</h4>
                         <img className="food-img" src={`/food${i}.png`} alt={product} />
                         {/* <Image className="food-img" src={이미지} width={500} height={500} /> */}
+
+                        <span> {count} </span>
+                        <button onClick={up}>+</button>
+                        <button onClick={down}>-</button>
                     </div>
                 )
             })}
