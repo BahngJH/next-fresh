cf149483108d30b1732ffa47c4dd0c7c26e67ef6
Bahng Ji Hoon <wpxm2003@naver.com>
2025-07-12 10:10:24 +0900

    cart 페이지 구성성

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,7 +1,28 @@
+import { age, name } from './data';
+
 export default function Cart() {
+    let 장바구니 = ['tomato', 'pasta'];
+    
     return (
-        <div>
-            <h1>장바구니 입니다.</h1>
-        </div>
+      <div>
+        <h4 className="title">Cart</h4>
+
+        {장바구니.map((name, i ) => {
+            return (
+                <CartItem key={i} name={name}/>
+            )
+        })}
+        
+      </div>
     )
-}
+  }
+  
+  function CartItem(props) {
+    return (
+      <div className="cart-item">
+        <p>상품명 {props.name} </p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+    )
+  }
