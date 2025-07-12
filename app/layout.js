69255adb05c0532c74a7a75db055f666aa9ed69d
Bahng Ji Hoon <wpxm2003@naver.com>
2025-07-12 16:15:10 +0900

    화면 정리리

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,11 @@ export default function RootLayout({ children }) {
           <Link href="/list">List</Link>
           <Link href="/cart">Cart</Link>
         </div> */}
+        <div className="navbar">
+          <Link href="/" className="logo">Home</Link>
+          <Link href="/list">List</Link>
+          <Link href="/write">Write</Link>
+        </div>
         {children}
       </body>
     </html>
